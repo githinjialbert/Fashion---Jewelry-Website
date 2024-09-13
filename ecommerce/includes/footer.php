edb3286f63d1dcccc7deb203fcde4c8cfebee012
githinjialbert <githinjialbert001@gmail.com>
2024-09-13 13:42:54 +0300

    finalizing the footer section

--- ecommerce/includes/footer.php
+++ ecommerce/includes/footer.php
@@ -3,10 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>headerSection</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <title>Footer Section</title>
 </head>
 <body>
+    <footer>
+        <img src="../assets/images/elfrida.jpg" alt="Elfrida Logo" id="elfrida-logo">
+        <h4>USER INTERFACE DESIGN, INTERACTION, DESIGN SYSTEMS<br>& ART DIRECTION.</h4>
+        <div class="partitions">
+            <div class="partition"><h4>FREE STANDARD DELIVERY</h4></div>
+            <div class="partition"><h4>100% SECURE PAYMENTS</h4></div>
+            <div class="partition"><h4>CUSTOMER SUPPORT</h4></div>
+            <div class="partition"><h4>FREE & EASY RETURNS</h4></div>
+        </div>
+        <section class="details-section">
+            <div class="details">
+                <h3>BE THE FIRST TO KNOW ABOUT <br> EVENTS, NEW PRODUCTS & OFFERS.</h3>
+                <p>*Subscribe to our Newsletter and get 15% off</p>
+                <input type="email" name="email" placeholder="Enter your email" id="input-footer">
+            </div>
+            <div class="details">
+                <h3>ABOUT US</h3>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </div>
+            <div class="details">
+                <h3>HELP</h3>
+                <ul>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Cart</a></li>
+                    <li><a href="#">My Account</a></li>
+                    <li><a href="#">Checkout</a></li>
+                </ul>
+            </div>
+        </section>
+        <div id="last-item">
+          <p>© 2024 Albert Interactive Designs, All Rights Reserved</p>  
+        </div>
+    </footer>
     <script src="../assets/js/app.js"></script>
 </body>
 </html>
