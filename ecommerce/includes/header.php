8cdb6474503a9ee9dda16cc17216189298a76526
githinjialbert <githinjialbert001@gmail.com>
2024-09-13 11:48:17 +0300

    updating the file paths

---
+++ ecommerce/includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>headerSection</title>
+</head>
+<body>
+    <header>
+        <nav>
+        <a href="#" target="" class="navigation">Home</a>
+        <a href="#" target="" class="navigation">Shop</a>
+        <a href="#" target="" class="navigation">About Us</a>
+        <a href="#" target="" class="navigation">Contact Us</a>
+        <a href="#" target="" class="navigation">My Account</a>
+        <a href="#" target="" class="navigation">Cart</a>
+        </nav>
+    </header>
+    <script src="/ecommerce/assets/js/app.js"></script>
+</body>
+</html>
