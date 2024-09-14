f6b2a028bd771212aa9760ea8808c5f2ea35bca1
githinjialbert <githinjialbert001@gmail.com>
2024-09-14 15:34:42 +0300

    finishing up the home page

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -1,2 +1,85 @@
-<?php 
-
+<?php require_once "./includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>HomePage</title>
+</head>
+<body>      
+    <main class="main-section">
+        <div class="main-imgsec">
+            <button id="home-button-explore">EXPLORE THE CAMPAIGN</button>
+        </div>
+        <div class="product-section1">
+            <h2>ELEGANCE EXPRESS:<br>CLASSIC AND<br>CONTEMPORARY STYLES</h2>
+            <div class="product-section">
+                <div class="product-sec">
+                    <img src="./assets/images/dragonearrings.jpg" alt="dragon-earrings"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/bronzenecklace.jpg" alt="bronze-necklace"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/necklace.jpg" alt="necklace"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/weddingring.jpg" alt="wedding-ring"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+            </div>
+        </div>
+        <div class="info-section">
+            <section class="info">
+                <h2>STEP INTO OUR FASHION BOTIQUE WHERE<br> ELEGANCE MEETS TRENDSETTING STYLE</h2>
+                <p>Offering a curated collection of haute couture, timeless classics,<br> and must-have accessories designed to elevate your wardrobe<br>
+            and celebrate your unique sense of fashion.
+                </p>
+                <button id="discover">DISCOVER MORE</button>
+            </section>
+            <section class="info">
+                <img src="./assets/images/bag_carrying.jpg" alt="bag_carrrying" id="bag_carrying"/>
+            </section>
+        </div>
+        <div class="product-section1">
+            <h2>NEW ARRIVALS SPRING SUMMER</h2>
+            <div class="product-section">
+                <div class="product-sec">
+                    <img src="./assets/images/bordeux.jpg" alt="dragon-earrings"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/redbag.jpg" alt="bronze-necklace"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/purplepurse.jpg" alt="necklace"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/pinkhandbag.jpg" alt="wedding-ring"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/yellowbag.jpg" alt="wedding-ring"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+                <div class="product-sec">
+                    <img src="./assets/images/partypurse.jpg" alt="wedding-ring"/>
+                    <button id="add-to-cart">ADD TO CART</button>
+                </div>
+            </div>
+        </div>
+        <div class="design-section">
+            <h1>Must-have accessories designed to elevate your wardrobe<br> and celebrate your unique sense of fashion<h1>
+            <button id="home-button-explore">DISCOVER</button>
+        </div>
+    </main>
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
+<?php require_once "./includes/footer.php" ?>
