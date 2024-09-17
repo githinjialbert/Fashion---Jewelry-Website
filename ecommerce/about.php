8325bf511c35d3ac70f8dabb4c9c458bdea6a9ac
githinjialbert <githinjialbert001@gmail.com>
2024-09-17 23:40:31 +0300

    adding the code for about page

---
+++ ecommerce/about.php
@@ -0,0 +1,44 @@
+<?php require_once "./includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>About Us</title>
+</head>
+<body>
+    <section class="about">
+        <h1>ABOUT US</h1>
+    </section>
+
+    <section class="info-section">
+        <div class="info">
+            <h2>STEP INTO OUR FASHION BOTIQUE WHERE<br> ELEGANCE MEETS TRENDSETTING STYLE</h2>
+            <p>Offering a curated collection of haute couture, timeless classics,<br> and must-have accessories designed to elevate your wardrobe<br>and celebrate your unique sense of fashion.</p>
+            <button id="discover">DISCOVER MORE</button>
+        </div>
+        <div class="info">
+            <img src="./assets/images/orange.jpg" alt="orange" id="orange"/>
+        </div>
+    </section>
+
+    <section class="info-section1">
+        <div class="info">
+            <h2>STEP INTO OUR FASHION BOTIQUE WHERE<br> ELEGANCE MEETS TRENDSETTING STYLE</h2>
+            <p>Offering a curated collection of haute couture, timeless classics,<br> and must-have accessories designed to elevate your wardrobe<br>and celebrate your unique sense of fashion.</p>
+            <button id="discover">DISCOVER MORE</button>
+        </div>
+        <div class="info">
+            <img src="./assets/images/bag_carrying.jpg" alt="bag_carrrying" id="bag_carrying"/>
+        </div>
+    </section>
+
+    <section class="blog-section">
+        <h2>STYLE STORIES: INSIDE THE FASHION STORE</h2>
+        <div class="blog" id="blog-sec"></div>
+    </section>
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
+<?php require_once "./includes/footer.php" ?>
