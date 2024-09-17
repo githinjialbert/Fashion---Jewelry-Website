15dbbfd034ecd3dd72fdc942d8dfff0e08b68787
githinjialbert <githinjialbert001@gmail.com>
2024-09-17 23:52:31 +0300

    adding the contact.php file

---
+++ ecommerce/contact.php
@@ -0,0 +1,14 @@
+<?php require_once "./includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>Contact Us</title>
+</head>
+<body>
+    <script src="./assets/css/styles.css"></script>
+</body>
+</html>
+<?php require_once "./includes/footer.php" ?>
