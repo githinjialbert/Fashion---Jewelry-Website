bafd600383de460daf25811c669e44febfea904d
githinjialbert <githinjialbert001@gmail.com>
2024-09-19 11:07:54 +0300

    shop cart code

---
+++ ecommerce/cart/shop.php
@@ -0,0 +1,30 @@
+<?php require_once "../includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <title>SHOP</title>
+</head>
+<body>
+
+    <section class="product-section2">
+        <div id="product-section3">
+        </div>
+    </section>
+
+    <section class="product-section2">
+        <div id="product-section3">
+        </div>
+    </section>
+
+    <section class="product-section2">
+        <div id="product-section3">
+        </div>
+    </section>
+
+    <script src="../assets/js/app.js"></script>   
+</body>
+</html>
+<?php require_once "../includes/footer.php" ?>
