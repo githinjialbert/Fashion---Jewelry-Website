472c7d144204bb962b75e3a84ef38904e8391076
githinjialbert <githinjialbert001@gmail.com>
2024-09-19 17:38:31 +0300

    building the cart page

---
+++ ecommerce/view_cart.php
@@ -0,0 +1,18 @@
+<?php require_once "./includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <title>Cart</title>
+</head>
+<body>
+    <main class="main-section-cart">
+        <div id="cart-products">Your cart is currently empty.</div>
+        <button type="submit" id="button">RETURN TO SHOP</button>
+    </main>
+    <script src="./assets/js/app.js"></script>    
+</body>
+</html>
+<?php require_once "./includes/footer.php" ?>
